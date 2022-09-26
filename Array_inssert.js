6dece4db298ae52982d56df4b8d4ab146685d248
sajibsaha4647 <sksaha4648@gmail.com>
2022-09-26 17:03:56 +0600

    updated

---
+++ Array_inssert.js
@@ -0,0 +1,24 @@
+let array = [30, 40, 50, 80];
+
+let element = 90;
+let pos = 0;
+
+// array.forEach((item, i) => {
+//   if (i <= pos) {
+//     array[i + 1] = array[i];
+//     if (i == pos) {
+//       array[i] = element;
+//     }
+//   }
+// });
+
+for (let i = array.length - 1; i >= 0; i--) {
+  if (i >= pos) {
+    array[i + 1] = array[i];
+  }
+  if (i == pos) {
+    array[i] = element;
+  }
+}
+
+console.log(array);
--- Array_traversing.js
+++ Array_traversing.js
@@ -1,1 +1,8 @@
-console.log("12");
+let array = [12, 34, 45, 45, 6, 4];
+
+array.forEach((num, i) => {
+  console.log(num);
+});
+// let named = "";
+// var name = prompt("What is your name?");
+// var num = prompt("What is your favorite number? ");
