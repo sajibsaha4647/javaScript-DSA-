8ce0dbee40f8e9544ec5cf7db6568d7ace005f20
sajibsaha4647 <sksaha4648@gmail.com>
2022-09-26 18:44:52 +0600

    array search

--- Array_search.js
+++ Array_search.js
@@ -13,4 +13,24 @@ for (let i = 0; i < array.length; i++) {
   }
 }
 
-console.log(array);
+// console.log(array);
+
+//search array index
+let array1 = [45, 40, 56, 67, 68, 78, 7, 56];
+
+let ele = 67;
+let index = null;
+
+for (let i = 0; i < array1.length; i++) {
+  if (array1[i] === ele) {
+    index = i;
+    break;
+  }
+}
+
+// let res = array1.splice(2, 1); //first index and next how many items [56]
+let res = array1.slice(1, 3); //start point from 1 index and finish at 3 index [40,56]
+
+console.log(res);
+
+console.log(index);
