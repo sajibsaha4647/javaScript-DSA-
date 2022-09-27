d5058f388b0de8416bc251429b39c695e169af87
sajibsaha4647 <sksaha4648@gmail.com>
2022-09-27 16:05:37 +0600

    bug fix in flight filter

--- Indirect_Recursion_Data.js
+++ Indirect_Recursion_Data.js
@@ -1,1 +1,17 @@
-indirectRecursion = () => {};
+let money = 100;
+let totalp = 0;
+
+BuyToalP = (y) => {
+  if (y > 0) {
+    MoorBuyToalP(y);
+    console.log("money " + y + " appple " + totalp);
+  } else {
+    console.log("can not buy more than" + totalp + " appple");
+  }
+};
+MoorBuyToalP = (y) => {
+  BuyToalP(y - 10);
+  totalp++;
+};
+
+console.log(BuyToalP(money));
