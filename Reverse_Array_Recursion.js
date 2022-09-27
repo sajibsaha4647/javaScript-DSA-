1a708badb6ebe84428c73f35c558453d74f41c05
sajibsaha4647 <sksaha4648@gmail.com>
2022-09-27 17:09:13 +0600

    bug fix in flight filter

---
+++ Reverse_Array_Recursion.js
@@ -0,0 +1,21 @@
+let array2 = [11, 21, 31, 41, 51, 61, 71];
+
+reversearray = (data, start, end) => {
+  //recursive with bubble sort
+  if (start <= end) {
+    console.log(start);
+    console.log(end);
+    console.log("===");
+
+    let temp = data[start];
+    data[start] = data[end];
+    data[end] = temp;
+    reversearray(data, start + 1, end - 1);
+  }
+};
+
+reversearray(array2, 0, array2.length - 1);
+
+// console.log(array2);
+let item = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(item.reverse());
