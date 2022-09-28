bd692b1dad193b1649a46167dc0a1362bcb34679
sajibsaha4647 <sksaha4648@gmail.com>
2022-09-28 14:25:42 +0600

    queue javascript

--- BinarySearch.js
+++ BinarySearch.js
@@ -1,1 +1,35 @@
-let array = [12, 34, 34, 4, 5, 56, 67, 56, 4, 43];
+//Binary always working on sorted array
+//so first sort array and then apply binary search
+// then execute then function
+// let array = [5, 9, 13, 17];
+
+// //now sorting the array first
+// for (let i = 0; i < array.length; i++) {
+//   for (let j = 0; j < array.length; j++) {
+//     if (array[j] > array[j + 1]) {
+//       let temp = array[j];
+//       array[j] = array[j + 1];
+//       array[j + 1] = temp;
+//     }
+//   }
+// }
+
+let data = [5, 9, 13, 17, 80, 85, 90];
+let find = 90;
+let start = 0;
+let end = data.length - 1;
+let position = undefined;
+
+while (start <= end) {
+  let mid = Math.floor((start + end) / 2);
+  if (data[mid] === find) {
+    position = mid;
+    break;
+  } else if (data[mid] < find) {
+    start = mid + 1;
+  } else {
+    end = mid - 1;
+  }
+}
+
+console.log(position);
