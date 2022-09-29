c7c115d21b3a0411d8ce446c89a9c42e59919113
sajibsaha4647 <sksaha4648@gmail.com>
2022-09-29 12:42:40 +0600

    sorting array optimized

---
+++ anagramString.js
@@ -0,0 +1,20 @@
+const checkanaGram = (str1, str2) => {
+  let obj1 = {};
+
+  if (str1.length != str2.length) {
+    return false;
+  }
+  for (ch of str1) {
+    obj1[ch] = obj1[ch] ? obj1[ch] + 1 : 1;
+  }
+
+  for (ch of str2) {
+    if (!obj1[ch]) {
+      return false;
+    }
+    obj1[ch]--;
+  }
+  return true;
+};
+
+console.log(checkanaGram("hellow", "ellowh"));
