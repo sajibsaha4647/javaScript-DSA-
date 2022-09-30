7a05e494770634910d4411ec57350df7e46f9b30
sajibsaha4647 <sksaha4648@gmail.com>
2022-09-30 17:45:55 +0600

    add javascript linkedlist

--- javascriptLinkedlist.js
+++ javascriptLinkedlist.js
@@ -13,29 +13,50 @@ class Linkedlist {
     let newdata = {
       value: newnode,
       next: null,
-    };
+    }
+   
     this.tail.next = newdata;
     this.tail = newdata;
     this.size += 1;
   }
 
   traversing = () => {
+
     let counter = 0;
-    let currentnode = this.head.next;
+    let currentnode = this.head;
 
     while (counter < this.size) {
-      console.log(JSON.stringify(currentnode));
-      currentnode = currentnode;
+      currentnode = currentnode.next;
       counter++;
     }
-  };
+  }
+
+  deleteElement = (index) =>{
+    let counter = 1;
+    let lead = this.head
+    if(index == 1){
+      this.head = this.head.next
+    }else{
+      while(counter<index-1){
+        lead = lead.next
+        counter++
+      
+      }
+    }
+    let newnode = lead.next.next
+    lead.next = newnode
+   
+
+  }
+
 }
 
-let list = new Linkedlist(200);
-list.appendform(2000);
-list.appendform(3000);
-list.appendform(4000);
-list.appendform(5000);
-list.appendform(4000);
+let list = new Linkedlist(1);
+list.appendform(2);
+list.appendform(3);
+list.appendform(4);
+list.appendform(5);
+list.appendform(60);
 list.traversing();
-// console.log(JSON.stringify(list));
+list.deleteElement(5);
+console.log(JSON.stringify(list));
