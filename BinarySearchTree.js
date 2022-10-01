84318eff4087222a175f5b72404816e0161c1a40
sajibsaha4647 <sksaha4648@gmail.com>
2022-10-01 13:32:09 +0600

    add deepcopyandshallowcopy

---
+++ BinarySearchTree.js
@@ -0,0 +1,27 @@
+class Node{
+
+    constructor(val){
+        this.value = val
+        this.left = null
+        this.right = null
+    }
+
+}
+
+class BinarySearchTree {
+
+    constructor(){
+        this.root == null
+    }
+
+    isrootnull = ()=>{
+
+    }
+
+
+}
+
+
+let bst = BinarySearchTree()
+
+let node = Node()
