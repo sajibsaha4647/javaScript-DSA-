67d769e7d1cc711bb8c032a14c18160c009b1184
sajibsaha4647 <sksaha4648@gmail.com>
2022-10-03 14:42:37 +0600

    Binary search maxvalue and minvalue

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -56,7 +56,7 @@ class BinarySearchTree {
   preOrder = (root) => {
     //start from root to end with left value first
     if (root !== null) {
-      console.log(root.value, "val pre");
+      // console.log(root.value, "val pre");
       this.preOrder(root.left);
       this.preOrder(root.right);
     }
@@ -77,7 +77,47 @@ class BinarySearchTree {
     if (root !== null) {
       this.postOrder(root.left);
       this.postOrder(root.right);
-      console.log(root.value);
+      // console.log(root.value);
+    }
+  };
+
+  bfsTraversion = (root) => {
+    //breath first search
+    let queue = [];
+    queue.push(this.root);
+    while (queue.length) {
+      var current = queue.shift();
+
+      if (current.left) {
+        queue.push(current.left);
+      }
+      if (current.right) {
+        queue.push(current.right);
+      }
+      // console.log(queue);
+      // console.log("==============");
+      // console.log(current.value);
+      //[20,10,30,5,11,23,40]
+    }
+  };
+
+  getMinValue = (root) => {
+    if (root !== null) {
+      if (root.left !== null) {
+        this.getMinValue(root.left);
+      } else {
+        console.warn(root.value, "this is minimum number");
+      }
+    }
+  };
+
+  getMaxvalue = (root) => {
+    if (root !== null) {
+      if (root.right !== null) {
+        this.getMaxvalue(root.right);
+      } else {
+        console.warn(root.value, "this is maximum number");
+      }
     }
   };
 }
@@ -86,6 +126,7 @@ let bst = new BinarySearchTree();
 bst.makeTree(20);
 bst.makeTree(10);
 bst.makeTree(5);
+bst.makeTree(2);
 bst.makeTree(11);
 bst.makeTree(30);
 bst.makeTree(40);
@@ -93,6 +134,9 @@ bst.makeTree(23);
 bst.preOrder(bst.root);
 bst.inOrder(bst.root);
 bst.postOrder(bst.root);
+bst.bfsTraversion(bst.root);
+bst.getMinValue(bst.root);
+bst.getMaxvalue(bst.root);
 // console.log(bst.searchNode(bst.root, 900));
 
 // console.log(JSON.stringify(bst), "full class");
