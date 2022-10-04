17938aa9e561c4752bc749b4901605f800493dc5
sajibsaha4647 <sksaha4648@gmail.com>
2022-10-04 14:32:23 +0600

    Adjacency graph make page change add delete and adges and vertex

--- javascriptMakeGraph.js
+++ javascriptMakeGraph.js
@@ -26,6 +26,31 @@ class Graph {
     this.data[v1].push(v2);
     this.data[v2].push(v1);
   };
+
+  RemoveEdges = (v1, v2) => {
+    if (!this.data[v1] || !this.data[v2]) {
+      console.log("Wrong value !");
+    } else {
+      this.data[v1] = this.data[v1].filter((item) => {
+        return item != v2;
+      });
+      this.data[v2] = this.data[v2].filter((item) => {
+        return item != v1;
+      });
+    }
+  };
+
+  RemoveVertex = (vertex) => {
+    if (!this.data[vertex]) {
+      console.log("Wrong value !");
+    } else {
+      for (let item of this.data[vertex]) {
+        this.RemoveEdges(vertex, item);
+        //first deleted vertex "A" and with items like [B,C]
+      }
+      delete this.data[vertex];
+    }
+  };
 }
 
 let graph = new Graph();
@@ -37,6 +62,11 @@ graph.addAdge("A", "C");
 graph.addAdge("A", "B");
 graph.addAdge("B", "C");
 graph.addAdge("B", "D");
+// graph.RemoveEdges("A", "B");
+// graph.RemoveEdges("C", "B");
+// graph.RemoveEdges("D", "B");
+// graph.RemoveEdges("C", "A");
+graph.RemoveVertex("A");
 console.log(graph);
 //Ans
 // data: {
