4d08bcc5e137c7f1d87ef9c65ef57a09c854d81e
sajibsaha4647 <sksaha4648@gmail.com>
2022-10-04 15:02:55 +0600

    javascript string question

---
+++ javascriptStringQuestions.js
@@ -0,0 +1,26 @@
+//javascript string questions
+
+let string = "hello how are you";
+
+// console.log(string.split(""));
+// [
+//     'h', 'e', 'l', 'l', 'o',
+//     ' ', 'h', 'o', 'w', ' ',
+//     'a', 'r', 'e', ' ', 'y',
+//     'o', 'u'
+//   ]
+// console.log([string]); //[ 'hello how are you' ]
+
+// console.log(string.replace(/h/g, "_")); //print _ello _ow are you
+
+// console.log(string.substring(6, 10)); //remove char how
+
+// //remove first char from the text
+// console.log(string.substring(1, string.length));
+
+// //remove last char from the text
+// console.log(string.substring(0, string.length - 1));
+
+// console.log(string.split("").reverse().join("")); // reverse string
+
+console.log(string.trim());
