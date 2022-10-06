84103347783591080b801adf92bd076c8ab37cc2
sajibsaha4647 <sksaha4648@gmail.com>
2022-10-06 13:55:55 +0600

    javascript completed questions

--- javascriptAdvanced.js
+++ javascriptAdvanced.js
@@ -174,7 +174,144 @@
 // console.log(data % 2);
 
 // 31
-let data = { nam: "sajib" };
-//deleted object values
-delete data.nam;
-console.log(data);
+// let data = { nam: "sajib" };
+// //deleted object values
+// delete data.nam;
+// console.log(data);
+
+//32
+// let data = "string";
+// //make this value false
+// console.log(!data);
+
+//33
+//what is the difference between foreach and map
+//map return always and foreach not return this is the main fact
+
+//34
+// let data = ["sajib", "saha", "rony", "rajib"];
+// delete data[1];
+// console.log(data);
+
+//35
+// let data = 3 ** 3; //this is squre of 3
+// console.log(data);
+//27
+
+//36
+// let data = 2;
+// setTimeout(() => {
+//   console.log(data);
+// }, 0);
+// data = 100;
+//100
+
+//37
+// let a = "sajib";
+// let b = `sajib`;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(a === b); //true
+
+//38
+// let a = 1;
+// let b = 2;
+// console.log(--b === a);
+//true
+
+// 39
+// let a = 1;
+// let b = 1;
+// let c = 2;
+// console.log((a === b) === --c);
+//alreadly compare to a and b and then get true now true and --c so that its false
+//false
+
+//40
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 1);
+// }
+// //3,3,3
+// for (let i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 1);
+// }
+// //1,2,3
+
+//41
+// getage = () => {
+//   age = 20;
+//   console.log(age);
+// };
+// getage(); //20
+
+// 42
+//here needs to be define variable as let const or var
+// getage2 = () => {
+//   "use strict";
+//   age = 20;
+//   console.log(age);
+// };
+// getage2(); //error
+
+//43
+// const sum = eval("10*10*5");
+// //convert into number
+// console.log(typeof sum);
+// console.log(sum);
+
+//44
+// let person = { name: "anil" };
+
+// sayhi = (age) => {
+//   return "name is " + person.name + "age is " + age;
+// };
+
+// console.log(sayhi.call(person, 21));
+// console.log(sayhi.bind(person, 21));
+// //return a function
+// console.log(sayhi.bind(person, 21)());
+// name is anilage is 21
+// name is anilage is 21
+
+//45
+// sayhi = () => {
+//   // return  0;//number
+//   // return () => 0; //function
+// };
+
+// console.log(typeof sayhi());
+
+//46
+// let timerID = setImmediate(() => {
+//   console.log("Hello, World");
+// });
+// console.log("Before the setImmediate call");
+// setTimeout(() => {
+//   console.log("Hello, World1");
+// }, 0);
+
+// console.log("After the setImmediate call");
+
+//47
+// const firstPromise = new Promise((res, rej) => {
+//   setTimeout(res, 500, "one");
+// });
+// const secondPromise = new Promise((res, rej) => {
+//   setTimeout(res, 100, "two");
+// });
+
+// Promise.race([firstPromise, secondPromise]).then((res) => {
+//   console.log(res);
+// });
+
+// 48
+// console.log(3 + 4 + "6"); //76 type string
+// console.log(3 + 4 + +"6"); //13 type number
+
+// 49
+// () => {
+//   let x = (y = 10);
+//   // let y = 12;
+// };
+// console.log(y);
+//y is not defined
